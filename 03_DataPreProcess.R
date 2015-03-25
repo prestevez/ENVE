6f9b56d396a6b1947573eb3de6935ff53baa19ae
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2015-03-25 20:14:37 +0000

    Work in progress

--- 03_DataPreProcess.R
+++ 03_DataPreProcess.R
@@ -4,9 +4,36 @@
 setwd(dirRdata)
 load("ENVE1214.Rdata")
 
+### Set factors as numeric
+
+# Create list of variables per data frame that need to be transformed
 
 
+### Set NAs
 
+# Should be same list as previous
+
+# mydata$v1[mydata$v1==99] <- NA, apply as needed?
+
+
+### Set factor labels
+# Create list of common factor relationships, levels labels pair
+
+# Alevels <- c(1,2,3,9) # includes "no aplica"
+# Alabels <- c("Sí", "No", "No aplica", "No sabe/no responde")
+
+# Create list of variables to apply such factors
+
+## Set logical variables, 9 is.na
+
+### Set ordered labels
+
+#### Create new indicator variables as needed
+
+# Create list of common ordered relationships, levels labels pair
+
+
+## Set variable labels Hmisc package
 
 
 
