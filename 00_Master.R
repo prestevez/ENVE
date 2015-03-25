a25270d52004f160e1da3e54d82dab8d300f7c67
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2015-03-25 16:31:59 +0000

    Updatted dir paths

--- 00_Master.R
+++ 00_Master.R
@@ -2,7 +2,9 @@
 
 # Register initial working directory
 
+setwd("/Users/prestevez/R Workspace/ENVE")
 MainWD <- getwd()
+
 
 # Create object of file where Log will be written 
 
@@ -14,16 +16,12 @@ sink(logfile, append=TRUE, type=c("output","message"))
 
 # Run the necessary scripts
 
-setwd(MainWD)
 source("01_Setup.R", echo=TRUE, max.deparse.length=10000)
 
-setwd(MainWD)
 source("02_LoadingData.R", echo=TRUE, max.deparse.length=10000)
 
-# setwd(MainWD)
 # source("03_XX.R", echo=TRUE, max.deparse.length=10000)
 # 
-# setwd(MainWD)
 # source("04_XX.R", echo=TRUE, max.deparse.length=10000)
 
 # End sink
--- 02_LoadingData.R
+++ 02_LoadingData.R
@@ -2,12 +2,23 @@
 
 setwd(dirRawData)
 
+# ## Read ENVE 2012 Main Questionnaire data
+# enve12 <- read.dbf("FILENAME.dbf")
+#
+# ## Read ENVE 2012 Victimisation module
+# vic12 <- read.dbf("FILENAME.dbf")
+# 
+# ## Read ENVE 2014 Main Questionnaire data
+# enve14 <- read.dbf("FILENAME.dbf")
+# 
+# ## Read ENVE 2012 Victimisation module
+# vic14 <- read.dbf("FILENAME.dbf")
 
+## Save objects for use latter
+setwd(dirRdata)
+save(list=ls(all=TRUE), file="ENVE1214.Rdata")
 
-
-
-
-
-
+# rm(list=ls(all=TRUE))
+# load("ENVE1214.Rdata")
 
 setwd(MainWD)
