af462b9ee4d796793b5eab07b543bdb8460b7180
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2015-03-25 15:28:58 +0000

    Added option to install and load packages from local files, left them commented out

--- 01_Setup.R
+++ 01_Setup.R
@@ -1,6 +1,6 @@
 ## Set up
 
-# Install and load packages
+# Install and load packages from the internet
 
 # install.packages("survey")
 # install.packages("ggplot2")
@@ -14,6 +14,26 @@ library(survey)
 library(foreign)
 # library(xtable)
 # library(ggplot2)
+
+# Install and load packages from local directory
+
+# dirPackages <- "Packages"
+# 
+# setwd(dirPackages)
+
+# install.packages("survey", lib=dirPackages)
+## install.packages("ggplot2", lib=dirPackages) # Should not be needed, it is in base install
+# install.packages("xtable", lib=dirPackages)
+# install.packages("foreign", lib=dirPackages)
+
+# Load packages into library
+
+# library(survey, lib.loc=dirPackages)
+## library(foreign) # foreign package should not be needed, it is in base install
+# library(xtable, lib.loc=dirPackages)
+# library(ggplot2, lib.loc=dirPackages)
+      
+# setwd(MainWD)
 
 # Set data paths
 
