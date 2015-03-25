80ff4b275dcf286f754e6c8dc9bfb12b6d31ac60
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2015-03-25 16:49:19 +0000

    Created 03_DataPreProcess R Script

---
+++ 03_DataPreProcess.R
@@ -0,0 +1,24 @@
+## Pre-process the data. Mostly change factors to numeric.
+
+setwd(dirRdata)
+load("ENVE1214.Rdata")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+save(list=ls(all=TRUE), file="ENVE1214.Rdata")
+
+setwd(MainWD)
+
+### END of 03_DataPreProcess.R
