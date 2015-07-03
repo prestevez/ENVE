e1a76660e4ab504ad2674beb8b4b4ccabe9e162a
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2015-07-03 20:19:58 +0100

    tests

--- 01_TestData_Manipulation.R
+++ 01_TestData_Manipulation.R
@@ -27,7 +27,8 @@ length(envipe.viv$ENT)
 
 ents <- envipe.viv[,c("CONTROL", "VIV_SEL", "ENT")]
 
-testset <- merge(testset, ents, by.x=c("control", "vivienda"), by.y=c("CONTROL", "VIV_SEL"))
+testset <- merge(testset, ents, by.x=c("control", "vivienda"), 
+                 by.y=c("CONTROL", "VIV_SEL"))
 
 testset$ENT <- as.integer(testset$ENT)
 
@@ -97,11 +98,39 @@ colnames(b_test) <- c("extortions", "bribes", "state", "state_code",
 levels(b_test$sector) <- c("retail", "services", "manufacturing", "office", "other")
 levels(b_test$size) <- c("small", "medium", "large")
 
+## Create a new b_test with better distributed covariates
 
+# b_test_backup <- b_test
+# b_test <- b_test_backup
+
+# years.b <- rpois(length(b_test$extortions), 5)
+# years.b[which(years.b == 0)] <- 1
+# 
+# sector.b <- sample(1:5, length(b_test$extortions), replace=TRUE)
+# size.b <- sample(1:3, length(b_test$extortions), replace=TRUE)
+# 
+# b_test$years <- years.b
+# b_test$sector <- as.factor(sector.b)
+# levels(b_test$sector) <- c("retail", "services", "manufacturing", "offices", "other")
+# b_test$size <- as.factor(size.b)
+# levels(b_test$size) <- c("small", "medium", "large")
+
+save(b_test, file="Rdata/modeldata.Rdata")
+
+## create a subset of b_test for test
+
+b_test_sample <- b_test[sample(nrow(b_test),10000),]
+
+table(b_test_sample$extortions)
+
+save(b_test_sample, file="Rdata/modeldata.Rdata")
+write.dta(b_test_sample, file = "b_test_sample.dta")
+
+b_test <- b_test_backup
 
 ## Save objects for use latter
 setwd(dirRdata)
-save(b_test, file="b_test.Rdata")
+save(list=ls(all=TRUE), file="b_test.Rdata")
 save(list=ls(all=TRUE), file="TestData.Rdata")
 
 setwd(MainWD)
