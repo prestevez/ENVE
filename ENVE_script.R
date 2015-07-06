00c49da32ea94056fea79bf8a958a7b81c1285cc
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2015-07-06 18:21:49 +0100

    Completed data manipulation

--- ENVE_script.R
+++ ENVE_script.R
@@ -20,17 +20,20 @@ Package_install(foreign)
 
 enve_all <- read.dbf("XXXXXX")
 cat_entidades <- read.csv("cat_entidades.csv", head=TRUE)
-homicidios <- read.csv("homicidios_tasas_2013.csv", header=TRUE)
+homicidios <- read.csv("homicidios_tasas_2013_b.csv", header=TRUE)
 homicidios <- merge(homicidios, cat_entidades, by="CVE_ENT")
-homicidios <- homicidios[,-2]
 
 # Prepare data for analysis
 
-enve_test <- data.frame(id=enve_all$i_cve)
+# Selecting oly the relevant variables
 
-enve_test$extortions <- as.numeric(as.character(enve_all$P26_10))
+enve_test <- data.frame(extortions=integer(), bribes=integer(), CVE_ENT=integer(),
+                          size=factor(), sector=factor(), susbector=factor(),
+                          years=integer())
 
-enve_test$bribes <- as.numeric(as.character(enve_all$P33))
+enve_test$extortions <- as.integer(as.character(enve_all$P26_10))
+
+enve_test$bribes <- as.integer(as.character(enve_all$P33))
 
 enve_test$CVE_ENT <- as.integer(enve_all$cve_ent)
 
@@ -41,3 +44,6 @@ enve_test$sector <- enve_all$Sector_final
 enve_test$subsector <- enve_all$P1_1B
 
 enve_test$years <- 2013 - as.numeric(as.character(enve_all$P3))
+# Still considering whether I should add one for 2013 businesses
+
+enve_test <- merge(enve_test, homicidios, by="CVE_ENT", all.x=TRUE)
