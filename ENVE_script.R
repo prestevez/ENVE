f3996b20cafbdc249c9d376c1b6279cb4ace61d3
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2015-07-06 20:15:17 +0100

    Advanced in the mean var relationship generated the poisson exp working on funs to test chi sq

--- ENVE_script.R
+++ ENVE_script.R
@@ -75,6 +75,20 @@ save(ext_dist, file=paste(dir_name, "ext_dist.Rdata", sep=""))
 
 # Testing for Poisson distribution
 
+# analysing the mean variance relationship
+
+mean_ext <- mean(enve_test$extortions)
+var_ext <- var(enve_test$extortions)
+
+mean_ext
+var_ext
+
+var_mean_ratio <- var_ext/mean_ext
+
+var_mean_ratio
+
+save(mean_ext, var_mean_ratio, var_ext, file=paste(dir_name, "var_mean.Rdata", sep=""))
+
 # Create DF to fit obs v expected for counts
 
 obsexp <- data.frame(Events=0:ext_dist[length(ext_dist$Events),1])
@@ -89,6 +103,40 @@ obsexp$Obs[is.na(obsexp$Obs)] <- 0
 
 obsexp
 
-# analysing the mean variance relationship
+obsexp$exp_po <- dpois(0:(length(obsexp$Events)-1), lambda=mean_ext)
 
-mean_ext <- mean(enve_test$extortions)
+## Function to print table with obs and exp and chi-sq test
+
+obs_exp_test <- function(dataframe, exp, par)
+  {
+  cs<-factor(0:(length(dataframe[,1])-1))
+  index <- max(which(exp >= 4))
+  levels(cs)[index:length(dataframe[,1]] <- paste(as.character(index-1), "+", sep="")
+  ef<-as.vector(tapply(exp,cs,sum))
+  of<-as.vector(tapply(dataframe[,2],cs,sum))
+  ofef_table <- data.frame(Events=0:(index-1), Obs=of, Exp=ef)
+  chisq_t <- sum((of-ef)^2/ef)
+  df <- length(of)-par-1
+  pval <- 1-pchisq(chisq_t, df)
+  return(list(Table=ofef_table, Chisq=chisq_t, DF=df, PValue=pval))
+  }
+
+## Convenience log function
+
+clog <- function(x) log(x + 1)
+
+## Same as above but on log scale
+
+obs_exp_test_log <- function(dataframe, exp, par)
+  {
+  cs<-factor(0:(length(dataframe[,1])-1))
+  index <- max(which(exp >= 4))
+  levels(cs)[index:length(dataframe[,1])] <- paste(as.character(index-1), "+", sep="")
+  ef<-as.vector(tapply(exp,cs,sum))
+  of<-as.vector(tapply(dataframe[,2],cs,sum))
+  ofef_table <- data.frame(Events=0:(index-1), log.Obs=clog(of), log.Exp=clog(ef))
+  chisq_t <- sum((clog(of)-clog(ef))^2/clog(ef))
+  df <- length(of)-par-1
+  pval <- 1-pchisq(chisq_t, df)
+  return(list(Table=ofef_table, Chisq=chisq_t, DF=df, PValue=pval))
+  }
