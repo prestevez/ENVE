be33cf7672375721716f30447af5aa1394ccee42
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2015-07-08 20:35:51 +0100

    Typo in messages

--- ENVE_script_00.R
+++ ENVE_script_00.R
@@ -30,7 +30,7 @@ logfile <- file(paste(dir_name, logfile_name, sep=""))
 
 # Initiate sink to store console output to log file
 
-sink(logfile, append=TRUE, type="messsage")
+sink(logfile, append=TRUE, type="message")
 sink(logfile, append=TRUE, type="output")
 
 # Run the scripts
@@ -39,7 +39,7 @@ source("ENVE_script.R", echo=TRUE, max.deparse.length=10000)
 
 # End sink
 
-sink(type="messsage")
+sink(type="message")
 sink(type="output")
 
 ## End of ENVE_script_00.R
