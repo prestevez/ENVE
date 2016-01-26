745f36c34ca02b69e569037df1b03fe3f20600fa
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2016-01-26 14:18:55 +0000

    almost done with aereal bribery section

--- ENVE_script_a.R
+++ ENVE_script_a.R
@@ -319,6 +319,7 @@ o4.0 <- glmmadmb(extortions ~ bribes + log(denuncias_homs) + log(poblacion) + ye
 summary(o4.0)
 confint(o4.0)
 dev.stat(o4.0)
+vif(o4.0)
 
 
 # Comparison between Poisson and NB GLMMs
@@ -395,135 +396,239 @@ dev.stat(o4.nb)
 ##########################################################################
 ##########################################################################
 
-# Second round using restbar ## Aqui me quede Use aereal bribery now
+# Models exploring aereal bribery
 
 ##########################################################################
 ##########################################################################
 ##########################################################################
 ##########################################################################
-
-# 1. Poisson GLM
-p1 <- glmmadmb(extortions ~ bribes + log(denuncias_homs) + log(poblacion) + yearsquant + restbar + size, data=enve_test,
-          family="poisson", zeroInflation=FALSE, extra.args="-ndi 60000",
-          admb.opts = admbControl(noinit = FALSE))
-
-summary(p1)
-confint(p1)
-dev.stat(p1)
-
-# 2. NB GLM
-
-p2 <- glmmadmb(extortions ~ bribes + log(denuncias_homs) + log(poblacion) + yearsquant + restbar + size, data=enve_test,
-          family="nbinom", zeroInflation=FALSE, extra.args="-ndi 60000",
-          admb.opts = admbControl(noinit = FALSE))
-
-summary(p2)
-confint(p2)
-dev.stat(p2)
-
-p2.1 <- glmmadmb(extortions ~ bribes + log(denuncias_homs) + log(poblacion) + yearsquant + restbar, data=enve_test,
-          family="nbinom", zeroInflation=FALSE, extra.args="-ndi 60000",
-          admb.opts = admbControl(noinit = FALSE))
-
-summary(p2.1)
-confint(p2.1)
-dev.stat(p2.1)
-
-
-p2.2 <- glmmadmb(extortions ~ bribes + log(denuncias_homs) + log(poblacion) + yearsquant, data=enve_test,
-          family="nbinom", zeroInflation=FALSE, extra.args="-ndi 60000",
-          admb.opts = admbControl(noinit = FALSE))
-
-summary(p2.2)
-confint(p2.2)
-dev.stat(p2.2)
-
-p2.3 <- glmmadmb(extortions ~ bribes + log(denuncias_homs) + log(poblacion), data=enve_test,
-          family="nbinom", zeroInflation=FALSE, extra.args="-ndi 60000",
-          admb.opts = admbControl(noinit = FALSE))
-
-summary(p2.3)
-confint(p2.3)
-dev.stat(p2.3)
-
-
-p2.4 <- glmmadmb(extortions ~ bribes, data=enve_test,
-          family="nbinom", zeroInflation=FALSE, extra.args="-ndi 60000",
-          admb.opts = admbControl(noinit = FALSE))
-
-summary(p2.4)
-confint(p2.4)
-dev.stat(p2.4)
-
-p2.null <- glmmadmb(extortions ~ 1, data=enve_test,
-          family="nbinom", zeroInflation=FALSE, extra.args="-ndi 60000",
-          admb.opts = admbControl(noinit = FALSE))
-
-summary(p2.null)
-confint(p2.null)
-dev.stat(p2.null)
-
-## Compare different nb models
-
-tx.p2.x <- texreg(list(p2.null, p2.4, p2.3, p2.2, p2.1, p2), caption="Comparison of all NB models",
-                  label="T_tx_p2x", booktabs=TRUE)
-
-tx.p2.x
-
-anova.p2.x <- anova(p2.null, p2.4, p2.3, p2.2, p2.1, p2)
-
-anova.p2.x
-
-xap2.x <- xtable(anova.p2.x, caption="ANOVA test between variables of the NB models", label="T_xap2x")
-
-# Compare NB and Poisson
-tx.p1_p2 <- texreg(list(p1, p2), caption="Comparison of Poisson and NB GLMs", label="T_n1n2", booktabs=TRUE)
-
-tx.p1_p2
-
-lr.p1_p2 <- lrtest(p1, p2)
-
-lr.p1_p2
-
-xlr.p1_p2 <- xtable(lr.p1_p2, caption="Likelihood ratio test between Poisson and NB GLMs", label="T_lrp1p2")
-
-print(xlr.p1_p2)
 
 # 3. Poisson GLMM
 
-p3 <- glmmadmb(extortions ~ bribes +  log(denuncias_homs) + log(poblacion) + yearsquant + restbar + size +
+p3.a <- glmmadmb(extortions ~ bribes +  tasahom + yearsquant + restbar + size + bribes_preval +
                 (1 | NOM_ABR), data=enve_test, family="poisson",
                zeroInflation=FALSE, extra.args="-ndi 60000",
                admb.opts = admbControl(noinit = FALSE))
 
-summary(p3)
-confint(p3)
-dev.stat(p3)
+summary(p3.a)
+confint(p3.a)
+dev.stat(p3.a)
+
+p3.b <- glmmadmb(extortions ~ bribes +  tasahom + yearsquant + restbar + size + bribes_inci +
+                (1 | NOM_ABR), data=enve_test, family="poisson",
+               zeroInflation=FALSE, extra.args="-ndi 60000",
+               admb.opts = admbControl(noinit = FALSE))
+
+summary(p3.b)
+confint(p3.b)
+dev.stat(p3.b)
+
+p3.c <- glmmadmb(extortions ~ bribes +  tasahom + yearsquant + restbar + size + bribes_conc +
+                (1 | NOM_ABR), data=enve_test, family="poisson",
+               zeroInflation=FALSE, extra.args="-ndi 60000",
+               admb.opts = admbControl(noinit = FALSE))
+
+summary(p3.c)
+confint(p3.c)
+dev.stat(p3.c)
 
 # 4. Negative Binomial GLMM
 
-p4.0 <- glmmadmb(extortions ~ bribes + log(denuncias_homs) + log(poblacion) + yearsquant + restbar + size +
+p4.a <- glmmadmb(extortions ~ bribes +  tasahom + yearsquant + restbar + size + bribes_preval +
                  (1 | NOM_ABR), data=enve_test,
                  family="nbinom", zeroInflation=FALSE, extra.args="-ndi 60000",
                  admb.opts = admbControl(noinit = FALSE))
 
-summary(p4.0)
-confint(p4.0)
-dev.stat(p4.0)
+summary(p4.a)
+confint(p4.a)
+dev.stat(p4.a)
+vif(p4.a)
+
+p4.b <- glmmadmb(extortions ~ bribes +  tasahom + yearsquant + restbar + size + bribes_inci +
+                 (1 | NOM_ABR), data=enve_test,
+                 family="nbinom", zeroInflation=FALSE, extra.args="-ndi 60000",
+                 admb.opts = admbControl(noinit = FALSE))
+
+summary(p4.b)
+confint(p4.b)
+dev.stat(p4.b)
+vif(p4.b)
+
+p4.c <- glmmadmb(extortions ~ bribes +  tasahom + yearsquant + restbar + size + bribes_conc +
+                 (1 | NOM_ABR), data=enve_test,
+                 family="nbinom", zeroInflation=FALSE, extra.args="-ndi 60000",
+                 admb.opts = admbControl(noinit = FALSE))
+
+summary(p4.c)
+confint(p4.c)
+dev.stat(p4.c)
+vif(p4.c)
 
 # Comparison between Poisson and NB GLMMs
 
-tx.p3_p4 <- texreg(list(p3, p4.0), caption="Comparison of Poisson and NB Mixed Models", label="T_m3m4", booktabs=TRUE)
+tx.p3_p4a <- texreg(list(p3.a, p4.a), caption="Comparison of Poisson and NB Mixed Models", label="T_m3m4", booktabs=TRUE)
 
-tx.p3_p4
+tx.p3_p4a
 
-lr.p3_p4 <- lrtest(p3, p4.0)
+lr.p3_p4a <- lrtest(p3.a, p4.a)
 
-lr.p3_p4
+lr.p3_p4a
 
-xlr.p3_p4 <- xtable(lr.p3_p4, caption="Likelihood ratio test between Poisson and NB Mixed Models", label="T_lrp3p4")
+xlr.p3_p4a <- xtable(lr.p3_p4a, caption="Likelihood ratio test between Poisson and NB Mixed Models", label="T_lrp3p4")
 
-print(xlr.p3_p4)
+print(xlr.p3_p4a)
+
+tx.p3_p4b <- texreg(list(p3.b, p4.b), caption="Comparison of Poisson and NB Mixed Models", label="T_m3m4", booktabs=TRUE)
+
+tx.p3_p4b
+
+lr.p3_p4b <- lrtest(p3.b, p4.b)
+
+lr.p3_p4b
+
+xlr.p3_p4b <- xtable(lr.p3_p4b, caption="Likelihood ratio test between Poisson and NB Mixed Models", label="T_lrp3p4")
+
+print(xlr.p3_p4b)
+
+tx.p3_p4c <- texreg(list(p3.c, p4.c), caption="Comparison of Poisson and NB Mixed Models", label="T_m3m4", booktabs=TRUE)
+
+tx.p3_p4c
+
+lr.p3_p4c <- lrtest(p3.c, p4.c)
+
+lr.p3_p4c
+
+xlr.p3_p4c <- xtable(lr.p3_p4c, caption="Likelihood ratio test between Poisson and NB Mixed Models", label="T_lrp3p4")
+
+print(xlr.p3_p4c)
+
+
+# compare nb using aereal bribery
+
+lr.p4a_p4b <- lrtest(p4.a, p4.b)
+
+lr.p4a_p4b
+
+lr.p4a_p4c <- lrtest(p4.a, p4.c)
+
+lr.p4a_p4c
+
+lr.p4c_p4b <- lrtest(p4.c, p4.b)
+
+lr.p4c_p4b
+
+## Drop 1 level bribery for each
+
+p4.a1 <- glmmadmb(extortions ~ tasahom + yearsquant + restbar + size + bribes_preval +
+                 (1 | NOM_ABR), data=enve_test,
+                 family="nbinom", zeroInflation=FALSE, extra.args="-ndi 60000",
+                 admb.opts = admbControl(noinit = FALSE))
+
+summary(p4.a1)
+confint(p4.a1)
+dev.stat(p4.a1)
+vif(p4.a1)
+
+p4.b1 <- glmmadmb(extortions ~ tasahom + yearsquant + restbar + size + bribes_inci +
+                 (1 | NOM_ABR), data=enve_test,
+                 family="nbinom", zeroInflation=FALSE, extra.args="-ndi 60000",
+                 admb.opts = admbControl(noinit = FALSE))
+
+summary(p4.b1)
+confint(p4.b1)
+dev.stat(p4.b1)
+vif(p4.b1)
+
+p4.c1 <- glmmadmb(extortions ~ tasahom + yearsquant + restbar + size + bribes_conc +
+                 (1 | NOM_ABR), data=enve_test,
+                 family="nbinom", zeroInflation=FALSE, extra.args="-ndi 60000",
+                 admb.opts = admbControl(noinit = FALSE))
+
+summary(p4.c1)
+confint(p4.c1)
+dev.stat(p4.c1)
+vif(p4.c1)
+
+### Compare these with the model without 1 and 2 level bribery (m4.1)
+
+lr.p4a1_m41 <- lrtest(p4.a1, m4.1)
+
+lr.p4a1_m41
+
+lr.p4b1_m41 <- lrtest(p4.b1, m4.1)
+
+lr.p4b1_m41
+
+lr.p4c1_m41 <- lrtest(p4.c1, m4.1)
+
+lr.p4c1_m41
+
+# compare with themselves
+
+lr.p4a1_p4b1 <- lrtest(p4.a1, p4.b1)
+
+lr.p4a1_p4b1
+
+lr.p4a1_p4c1 <- lrtest(p4.a1, p4.c1)
+
+lr.p4a1_p4c1
+
+lr.p4c1_p4b1 <- lrtest(p4.c1, p4.b1)
+
+lr.p4c1_p4b1
+
+## Now with raw state bribery aggregate
+
+p4.d <- glmmadmb(extortions ~ bribes + log(denuncias_homs) + log(poblacion) + yearsquant + restbar + size + bribes_abvic
+                    (1 | NOM_ABR), data=enve_test,
+                    family="nbinom", zeroInflation=FALSE, extra.args="-ndi 60000",
+                 admb.opts = admbControl(noinit = FALSE))
+
+summary(p4.d)
+confint(p4.d)
+dev.stat(p4.d)
+vif(p4.d)
+
+p4.e <- glmmadmb(extortions ~ bribes + log(denuncias_homs) + log(poblacion) + yearsquant + restbar + size + bribes_abincs
+                    (1 | NOM_ABR), data=enve_test,
+                    family="nbinom", zeroInflation=FALSE, extra.args="-ndi 60000",
+                 admb.opts = admbControl(noinit = FALSE))
+
+summary(p4.e)
+confint(p4.e)
+dev.stat(p4.e)
+vif(p4.e)
+
+# compare the two
+
+lr.p4d_p4e <- lrtest(p4.e, p4.d)
+
+lr.p4d_p4e
+
+# compare with bribery rates model
+
+lr.p4d_p4a <- lrtest(p4.a, p4.d)
+
+lr.p4d_p4a
+
+lr.p4e_p4b <- lrtest(p4.b, p4.e)
+
+lr.p4e_p4b
+
+## Drop one level bribery
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # 4.x Investigating different variables in the NB GLMMs
 
